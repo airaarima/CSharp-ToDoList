2cafbf196ff77304d6ca0b6a52b95500894eaac9
airaarima <arimacoelhoaira@gmail.com>
2025-04-28 11:49:56 -0300

    chore: ajusta namespace

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,36 +3,35 @@ using Microsoft.AspNetCore.Mvc;
 using ToDoList.DTOs;
 using ToDoList.Services;
 
-namespace ToDoList.Controllers
+namespace ToDoList.Controllers;
+
+[Route("api/user")]
+[ApiController]
+[AllowAnonymous]
+public class UserController : ControllerBase
 {
-    [Route("api/user")]
-    [ApiController]
-    [AllowAnonymous]
-    public class UserController : ControllerBase
+    private readonly IUserService _service;
+
+    public UserController(IUserService service)
     {
-        private readonly IUserService _service;
+        _service = service;
+    }
 
-        public UserController(IUserService service)
-        {
-            _service = service;
-        }
+    [HttpPost]
+    public async Task<IActionResult> CreateUserAsync(UserCreateDto user)
+    {
+        var response = await _service.CreateUserAsync(user);
+        if (response == 0) return BadRequest("Não foi possível criar o usuário.");
 
-        [HttpPost]
-        public async Task<IActionResult> CreateUserAsync(UserCreateDto user)
-        {
-            var response = await _service.CreateUserAsync(user);
-            if (response == 0) return BadRequest("Não foi possível criar o usuário.");
+        return Created();
+    }
 
-            return Created();
-        }
+    [HttpPost("login")]
+    public async Task<IActionResult> LoginAsync(UserLoginDto user)
+    {
+        var response = await _service.LoginAsync(user);
+        if (response == null) return BadRequest("Erro ao realizar login.");
 
-        [HttpPost("login")]
-        public async Task<IActionResult> LoginAsync(UserLoginDto user)
-        {
-            var response = await _service.LoginAsync(user);
-            if (response == null) return BadRequest("Erro ao realizar login.");
-
-            return Ok(new {accessToken = response});
-        }
+        return Ok(new {accessToken = response});
     }
 }
--- Utils/AuthService.cs
+++ Utils/AuthService.cs
@@ -3,36 +3,35 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using ToDoList.Models;
 
-namespace ToDoList.Utils
+namespace ToDoList.Utils;
+
+public class AuthService(IConfiguration configuration)
 {
-    public class AuthService(IConfiguration configuration)
+    private readonly IConfiguration _configuration = configuration;
+    public string GenerateJwtToken(User user)
     {
-        private readonly IConfiguration _configuration = configuration;
-        public string GenerateJwtToken(User user)
+        var claims = new[]
         {
-            var claims = new[]
-            {
-                new Claim(ClaimTypes.Name, user.UserName),
-                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
-            };
+            new Claim(ClaimTypes.Name, user.UserName),
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+        };
 
-            var keyBytes = Convert.FromBase64String(_configuration["JWT:SecretKey"]);
-            var signingKey = new SymmetricSecurityKey(keyBytes);
-            var creds = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
+        var keyBytes = Convert.FromBase64String(_configuration["JWT:SecretKey"]);
+        var signingKey = new SymmetricSecurityKey(keyBytes);
+        var creds = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
 
 
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.UtcNow.AddMinutes(30),
-                Issuer = _configuration["JWT:Issuer"],
-                Audience = _configuration["JWT:Audience"],
-                SigningCredentials = creds
-            };
-            
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            return tokenHandler.WriteToken(token);
-        }
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddMinutes(30),
+            Issuer = _configuration["JWT:Issuer"],
+            Audience = _configuration["JWT:Audience"],
+            SigningCredentials = creds
+        };
+        
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
     }
 }
