a00f9afa750f9f2e3c6865b2d3895942e6ac5373
airaarima <arimacoelhoaira@gmail.com>
2025-05-09 16:19:00 -0300

    refactor: atualiza nome de coluna na tabela toDo

---
+++ Migrations/20250509181709_updateNameColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateNameColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Data",
+                table: "ToDo",
+                newName: "Date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "ToDo",
+                newName: "Data");
+        }
+    }
+}
